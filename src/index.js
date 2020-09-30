86b56e3ad2d00f044ee30241dc9638da7a92b0b2
Denis <den_from_bykhov1988@mail.ru>
2020-09-30 23:09:42 +0300

    feat: add min, max and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (arguments.length < 1 || array.length < 1) {
+    return 0;
+  } else {
+      return array.sort((a, b) => {
+          return a - b
+      })[0];
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (arguments.length < 1 || array.length < 1) {
+        return 0;
+    } else {
+        return array.sort((a, b) => {
+            return b - a
+        })[0];
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (arguments.length < 1 || array.length < 1) {
+        return 0;
+    } else {
+        return array.reduce((prevValue, curValue) => {
+            return prevValue + curValue
+        }) / array.length;
+    }
 }
